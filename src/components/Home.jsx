6a940d19af105933d781bbe96c7012abfd114d4e
luckychappie <luckychappie000111@gmail.com>
2024-08-07 22:42:46 +0700

    Initial commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Container, Nav } from 'react-bootstrap'
+
+const Home = () => {
+  return (
+
+    <div>
+        <Nav>
+            
+        </Nav>
+        <Container>
+        <h3>Home page</h3>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. In, saepe! Consectetur natus numquam corrupti obcaecati saepe ipsum praesentium, excepturi esse tenetur eligendi quos corporis expedita fugiat, eius tempora nisi ducimus?</p>
+        </Container>
+        
+    </div>
+  )
+}
+
+export default Home
