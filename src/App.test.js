dd9b1e79fcd023745a50a9efc2445c25931437dd
luckychappie <luckychappie000111@gmail.com>
2024-08-08 00:00:41 +0700

    feature/SCRUM-3-test-CI-and-update-unit-test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/testing/i);
+  const linkElement = screen.getByText(/apple/i);
   expect(linkElement).toBeInTheDocument();
 });
